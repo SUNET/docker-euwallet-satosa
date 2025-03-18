e2fd3a7843733772b0f70e11fc6c7b02a2e5c4e5
Mikael Frykholm <mifr@sunet.se>
2025-03-18 15:12:58 +0100

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12-slim-bookworm
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
+RUN apt update && apt install -y --no-install-recommends git
+RUN --mount=type=bind,source=uv.lock,target=uv.lock \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    uv sync --frozen --no-install-project
+
+# Set language to prevent errors when breaking
+# into the container to run satosa-saml-metadata.
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+COPY start.sh /tmp/satosa/start.sh
+COPY attributemaps /tmp/satosa/attributemaps
+ENTRYPOINT ["/tmp/satosa/start.sh"]
---
+++ start.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# for Click library to work in satosa-saml-metadata
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+# exit immediately on failure
+set -e
+
+DATA_DIR=${DATA_DIR-/etc/satosa}
+METADATA_DIR=${METADATA_DIR-"${DATA_DIR}"}
+PROXY_PORT=${PROXY_PORT-8000}
+
+# Gunicorn options
+WORKERS=${WORKERS-4}
+WORKER_CLASS=${WORKER_CLASS-sync}
+WORKER_THREADS=${WORKER_THREADS-1}
+WORKER_TIMEOUT=${WORKER_TIMEOUT-60}
+
+if [ ! -d "${DATA_DIR}" ]; then
+   mkdir -p "${DATA_DIR}"
+fi
+
+cd ${DATA_DIR}
+
+mkdir -p ${METADATA_DIR}
+
+if [ ! -d ${DATA_DIR}/attributemaps ]; then
+   cp -pr /tmp/satosa/attributemaps ${DATA_DIR}/attributemaps
+fi
+
+
+# generate metadata for front- (IdP) and back-end (SP) and write it to mounted volume
+
+satosa-saml-metadata proxy_conf.yaml ${DATA_DIR}/metadata.key ${DATA_DIR}/metadata.crt --dir ${METADATA_DIR}
+
+# start the proxy
+if [[ -f https.key && -f https.crt ]]; then # if HTTPS cert is available, use it
+  exec gunicorn --reload --bind 0.0.0.0:${PROXY_PORT} --keyfile https.key --certfile https.crt \
+      --workers ${WORKERS} --worker-class ${WORKER_CLASS} --threads ${WORKER_THREADS} \
+      --timeout ${WORKER_TIMEOUT} satosa.wsgi:app
+else
+  exec gunicorn --bind 0.0.0.0:${PROXY_PORT} --workers ${WORKERS} --worker-class ${WORKER_CLASS} \
+      --threads ${WORKER_THREADS} --timeout ${WORKER_TIMEOUT} satosa.wsgi:app
+fi
